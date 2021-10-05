ab5d932a23aadccec35de4a35069aa3e9c7a35d9
Enzzza <44332939+Enzzza@users.noreply.github.com>
2021-10-05 21:57:54 +0200

    Ready for droplet

--- mailer/email_sender.py
+++ mailer/email_sender.py
@@ -6,79 +6,80 @@ import json
 from email.message import EmailMessage
 from dotenv import load_dotenv
 load_dotenv()
-
+ 
 EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_ADDRESS_FINAL = os.environ.get("EMAIL_ADDRESS_FINAL")
-
+ 
 def connect_to_db(dbName):
     global conn
     global c
     conn = sqlite3.connect(f"{dbName}.db")
     c = conn.cursor()
-
+ 
 def send_mail(subject, to_address, txt_content, html_content=None):
     msg = EmailMessage()
     msg['Subject'] = subject
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = to_address
-
-    
+ 
+ 
     msg.set_content(txt_content)
     if html_content is not None:
         msg.add_alternative(html_content, subtype='html')
-
-
+ 
+ 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
-
+ 
 def get_content(name):
-    
+ 
     if name is None:
         name = "there"
         footer_msg = "Thank you very much."
     else:
         footer_msg = f"Thank you, {name}."
-
+ 
     html_file = codecs.open("html_content.html", "r", "utf-8")
     html_content = html_file.read()
     html_content = html_content.replace("[name]", name)
     html_content = html_content.replace("[footer_msg]", footer_msg)
-    
+ 
     with open('txt_content.txt') as f:
         txt_content = f.read()
-    
+ 
     txt_content = txt_content.replace("[name]", name)
-    
+ 
     return txt_content, html_content
-    
+ 
 def get_username():
     c.execute("SELECT * FROM lawyers WHERE sent=0")
     values = c.fetchone()
     if values is not None:
         return values
-    
+ 
     return None
-
+ 
 def update_sent_status(id):
     with conn:
         c.execute("""UPDATE lawyers SET sent = 1
                     WHERE id = :id""",
                   {'id': id })
-
+ 
 def email_task():
-    connect_to_db("test_lawyers")
+    connect_to_db("lawyers")
     values = get_username()
     if values is not None:
         (id, name, email, _) = values
         txt_content, html_content = get_content(name)
         try:
             send_mail('Be part of future, AI Lawyer needs you!', email, txt_content, html_content)
+            print(f"Sending email to {email}")
         except:
             print("Something went wrong")
         else:
             update_sent_status(id)
     else:
         send_mail('[INFO] AI Lawyer mailer',EMAIL_ADDRESS_FINAL, "All mails sent!")
-        
+        print("All mails sent!")
--- mailer/main.py
+++ mailer/main.py
@@ -1,9 +1,36 @@
-from datetime import timedelta
+import redis
+from rq import Queue
+import sqlite3
+import humanize
+import datetime as dt
+import math
 from email_sender import email_task
-from redis import Redis
-from rq import Queue, queue
+ 
+def connect_to_db(dbName):
+    global conn
+    global c
+    conn = sqlite3.connect(f"{dbName}.db")
+    c = conn.cursor()
+ 
+def main():
+    connect_to_db("test_lawyers")
+    c.execute("SELECT * FROM lawyers WHERE sent=0")
+    num_of_mails = len(c.fetchall())
+    g_mail_limit = 500
+    duration_between = math.floor(24 * 60 * 60 / g_mail_limit)
+ 
+    print(f"TO send this mails it will take {humanize.naturaldelta(dt.timedelta(seconds=num_of_mails*duration_between))}")
+    r = redis.Redis()
+    q = Queue(connection=r)
+ 
+    if num_of_mails != 0:
+        for m in range(num_of_mails):
+            job = q.enqueue_in(dt.timedelta(seconds=duration_between*m), email_task)
+            print(job)
+ 
+    else:
+        job = q.enqueue_in(dt.timedelta(seconds=duration_between), email_task)
 
 
-q = Queue(connection=Redis(host='redis',port=6379))
-
-job = q.enqueue_in(timedelta(seconds=60),email_task)
+if __name__ == "__main__":
+    main()
